f57802f488edf23837fa09813a6d5f7763fc70c1
hiusbm01 <bominseo07@gmail.com>
2025-09-01 17:55:27 +0900

    feat: 배포 후 첫 수청

--- backend/src/main/java/com/stud/backend/config/SpringSecurityConfig.java
+++ backend/src/main/java/com/stud/backend/config/SpringSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -35,26 +36,32 @@ public class SpringSecurityConfig {
         http
                 // REST API이므로 CSRF 보호를 비활성화합니다.
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
 
                 // JWT 기반 인증을 사용하므로 세션을 사용하지 않습니다.
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 // HTTP 요청에 대한 인가 규칙을 설정합니다.
-                .authorizeHttpRequests(authorize -> authorize
+                .authorizeHttpRequests(authorize -> {
+                    System.out.println(">>>>CHECK CODE>>>>");
+                    authorize
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        //로그인 회원가입 접근가능
-                        .requestMatchers("/api/users/register", "api/users/login", "/ws/**").permitAll()
-                        //GET요청은 인증된 사용자에게 대부분허용
-                        .requestMatchers(HttpMethod.GET, "/api/**").authenticated()
-                        //POST 등 요청 명시적으로 추가해주는곳
-                        .requestMatchers(HttpMethod.POST, "/api/seats/**", "/api/tickets/**").authenticated()
-                        //관리자용 경로
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                        //나머지 모든 요청 일단 인증 필요하도록 설정
-                        .anyRequest().authenticated()
-                        
-                )
+
+                            .requestMatchers("/", "/index.html", "/favicon.ico", "/assets/**").permitAll()
+
+                            //로그인 회원가입 접근가능
+                            .requestMatchers("/api/users/register", "/api/users/login", "/ws/**").permitAll()
+                            //GET요청은 인증된 사용자에게 대부분허용
+                            .requestMatchers(HttpMethod.GET, "/api/public/**").permitAll()
+                            .requestMatchers(HttpMethod.GET, "/api/**").authenticated()
+                            //POST 등 요청 명시적으로 추가해주는곳
+                            .requestMatchers(HttpMethod.POST, "/api/seats/**", "/api/tickets/**").authenticated()
+                            //관리자용 경로
+                            .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                            //나머지 모든 요청 일단 인증 필요하도록 설정
+                            .anyRequest().authenticated();
+
+                })
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
@@ -63,7 +70,7 @@ public class SpringSecurityConfig {
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
         //프론트 서버 주소 허용
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","http://13.236.159.92"));
         //허용할 HTTP 메서드 설정
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
         //헤더 설정
--- stud-frontend/vite.config.js
+++ stud-frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api' :{
-        target: 'http://localhost:8080',
+        target: 'http://13.236.159.92:8080',
         changeOrigin: true,
       }
     }
