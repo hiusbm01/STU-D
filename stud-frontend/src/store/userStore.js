8ed42caa2807dac04bc9c9b79cf27bf8e43d1ae2
hiusbm01 <bominseo07@gmail.com>
2025-09-12 17:49:22 +0900

    feat:새로고침 로그인정보 초기화 해결

--- stud-frontend/src/store/userStore.js
+++ stud-frontend/src/store/userStore.js
@@ -1,27 +1,37 @@
 import { create } from 'zustand';
 import {jwtDecode} from 'jwt-decode';
+import { persist, createJSONStorage} from 'zustand/middleware';
 
-const useUserStore = create((set) => ({
-    user: null, //사용자 정보
-    token: null,
-    isLoggedIn: false, //로그인 상태
-    role: null,
+const useUserStore = create(
+    persist(
+        (set) => ({
+            user: null, //사용자 정보
+            token: null,
+            isLoggedIn: false, //로그인 상태
+            role: null,
 
-    login: (token) =>{
-        
-        const decoded = jwtDecode(token);
-        const userEmail = decoded.sub;
-        const userRole = decoded.auth;
+            login: (token) =>{
+                
+                const decoded = jwtDecode(token);
+                const userEmail = decoded.sub;
+                const userRole = decoded.auth;
 
-        set({
-            user: {email: userEmail},
-            token: token,
-            isLoggedIn: true,
-            role: userRole,
-        });
-    },
+                set({
+                    user: {email: userEmail},
+                    token: token,
+                    isLoggedIn: true,
+                    role: userRole,
+                });
+            },
 
-    logout: () => set({user : null, token: null, isLoggedIn:false, role: null}),
-}));
+            logout: () => set({user : null, token: null, isLoggedIn:false, role: null}),
+        }),
+        {
+            name: 'user-login-status',
+            storage: createJSONStorage(() => localStorage),
+        }
+
+    )
+);
 
 export default useUserStore;
