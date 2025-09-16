38830a6f3012a0fdf1768c345e9762625b70ab9d
hiusbm01 <bominseo07@gmail.com>
2025-09-17 01:09:38 +0900

    feat: 진짜 글꼴 수정

--- stud-frontend/src/main.jsx
+++ stud-frontend/src/main.jsx
@@ -4,10 +4,20 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 
+import { createTheme, ThemeProvider} from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily : "'Noto Sans KR', sans-serif",
+  }
+})
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <ThemeProvider theme={theme}>
+
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
   </React.StrictMode>,
 
